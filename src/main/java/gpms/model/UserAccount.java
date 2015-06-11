6f53aa79719c55c32da20dbbb0961532da6dda20
Milstein Munakami <milsonmun@yahoo.com>
2015-06-10 18:00:14 -0600

    added is deleted to UserAccount Model
    Signed-off-by: Milstein Munakami <milsonmun@yahoo.com>

--- src/main/java/gpms/model/UserAccount.java
+++ src/main/java/gpms/model/UserAccount.java
@@ -15,44 +15,48 @@ public class UserAccount extends BaseEntity {
 	private String userName;
 	@Property("password")
 	private String password;
+	@Property("is deleted")
+	private Boolean isDeleted;
 
-	public UserAccount(String userName, String password) {
+	public UserAccount(String userName, String password, Boolean isDeleted) {
 		this.userName = userName;
 		this.password = password;
+		this.isDeleted = isDeleted;
 		// TODO:: encrypt the password
 		// this.set_uid(id);
 	}
 
-	public UserAccount(String userName) 
-	{
+	public UserAccount(String userName) {
 		this.userName = userName;
 		this.password = "123456789"; // TODO:: user random password generator
 	}
 
-	public String getUserName() 
-	{
+	public String getUserName() {
 		return userName;
 	}
 
-	public void setUserName(String userName) 
-	{
+	public void setUserName(String userName) {
 		this.userName = userName;
 	}
 
-	public String getPassword() 
-	{
+	public String getPassword() {
 		return password;
 	}
 
-	public void setPassword(String password) 
-	{
+	public void setPassword(String password) {
 		this.password = password;
 	}
 
-	public UserAccount() 
-	{
-		
+	public Boolean getIsDeleted() {
+		return isDeleted;
 	}
 
+	public void setIsDeleted(Boolean isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+
+	public UserAccount() {
+
+	}
 
 }
