26e4bef994f5af69cbdb643f01e49e718c0bbb12
Milstein Munakami <milsonmun@yahoo.com>
2015-06-16 14:25:58 -0600

    added isDeleted, deletedBy, deletedOn and same thing for Update case

--- src/main/java/gpms/dao/BaseEntity.java
+++ src/main/java/gpms/dao/BaseEntity.java
@@ -1,5 +1,9 @@
 package gpms.dao;
 
+import gpms.model.UserAccount;
+
+import java.util.Date;
+
 import org.bson.types.ObjectId;
 import org.mongodb.morphia.annotations.Id;
 import org.mongodb.morphia.annotations.Property;
@@ -13,6 +17,13 @@ public abstract class BaseEntity {
 	@Version
 	@Property("version")
 	private Long version;
+
+	private Boolean isDeleted;
+	private Boolean isUpdated;
+	private Date deletedOn;
+	private Date updatedOn;
+	private UserAccount deletedBy;
+	private UserAccount updatedBy;
 
 	public BaseEntity() {
 		super();
@@ -29,4 +40,53 @@ public abstract class BaseEntity {
 	public void setVersion(Long version) {
 		this.version = version;
 	}
+
+	public Boolean getIsDeleted() {
+		return isDeleted;
+	}
+
+	public void setIsDeleted(Boolean isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+
+	public Boolean getIsUpdated() {
+		return isUpdated;
+	}
+
+	public void setIsUpdated(Boolean isUpdated) {
+		this.isUpdated = isUpdated;
+	}
+
+	public Date getDeletedOn() {
+		return deletedOn;
+	}
+
+	public void setDeletedOn(Date deletedOn) {
+		this.deletedOn = deletedOn;
+	}
+
+	public Date getUpdatedOn() {
+		return updatedOn;
+	}
+
+	public void setUpdatedOn(Date updatedOn) {
+		this.updatedOn = updatedOn;
+	}
+
+	public UserAccount getDeletedBy() {
+		return deletedBy;
+	}
+
+	public void setDeletedBy(UserAccount deletedBy) {
+		this.deletedBy = deletedBy;
+	}
+
+	public UserAccount getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(UserAccount updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
 }
