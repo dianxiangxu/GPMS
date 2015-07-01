6ff69a1fb4f230b295a514cfa7d47b51eca786d7
thomasvolz <thomasvolz@u.boisestate.edu>
2015-07-01 09:19:39 -0600

    Working on a couple of test cases

--- src/test/java/gpms/dao/test/SuiteForCRUDTest.java
+++ src/test/java/gpms/dao/test/SuiteForCRUDTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({JUnitCreateTwoUsersTest.class, JUnitAuditTest.class})
+@Suite.SuiteClasses({JUnitCreateTwoUsersTest.class, JUnitAuditTest.class,})
 public class SuiteForCRUDTest 
 {
 
