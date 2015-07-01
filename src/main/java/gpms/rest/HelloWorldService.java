84015dbd18756f2dd5f464012912f381832f75c7
Milstein Munakami <milsonmun@yahoo.com>
2015-07-01 14:15:04 -0600

    updated for JSON format output

--- src/main/java/gpms/rest/HelloWorldService.java
+++ src/main/java/gpms/rest/HelloWorldService.java
@@ -9,6 +9,8 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.fasterxml.jackson.jaxrs.json.annotation.JSONP;
+
 // Plain old Java Object it does not extend as class or implements 
 // an interface
 
@@ -30,20 +32,23 @@ public class HelloWorldService {
 	}
 
 	// This method is called if XML is request
+	@Path("{id}/xml")
 	@GET
 	@Produces(MediaType.TEXT_XML)
-	public String sayXMLHello() {
-		return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
+	public String sayXMLHello(@PathParam("id") String id) {
+		return "<?xml version=\"1.0\"?>" + "<hello> Hello " + id + "</hello>";
 	}
 
 	// This method is called if HTML is request
+	@Path("{id}/html")
 	@GET
 	@Produces(MediaType.TEXT_HTML)
-	public String sayHtmlHello() {
-		return "<html> " + "<title>" + "Hello Jersey" + "</title>"
-				+ "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
+	public String sayHtmlHello(@PathParam("id") String id) {
+		return "<html> " + "<title>" + "Hello " + id + "</title>"
+				+ "<body><h1>" + "Hello " + id + "</body></h1>" + "</html> ";
 	}
 
+	// Using Parameterized REST
 	@GET
 	@Path("/{parameter}")
 	public Response responseMsg(@PathParam("parameter") String parameter,
@@ -52,5 +57,13 @@ public class HelloWorldService {
 		String output = "Hello from: " + parameter + " : " + value;
 
 		return Response.status(200).entity(output).build();
+	}
+
+	// For JSON example
+	@GET
+	@Path("/GetAllProducts")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getAllProducts() {
+		return "{\"name\":\"MAC\", \"quantity\":\"10\"}";
 	}
 }
