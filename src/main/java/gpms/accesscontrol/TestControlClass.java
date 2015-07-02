5cf6611d00a89052bb731b2d93bbbbf83d6a703f
thomasvolz <thomasvolz@u.boisestate.edu>
2015-07-02 09:21:05 -0600

    Late commit

---
+++ src/main/java/gpms/accesscontrol/TestControlClass.java
@@ -0,0 +1,14 @@
+package gpms.accesscontrol;
+
+import org.wso2.balana.Balana;
+import org.wso2.balana.ctx.AbstractResult;
+
+public class TestControlClass 
+{
+	private static Balana balana = null;
+	private AbstractResult ar;
+	
+	Accesscontrol theTest;
+	
+	
+}
--- src/test/java/gpms/accesscontrol/EditUserTest.java
+++ src/test/java/gpms/accesscontrol/EditUserTest.java
@@ -1,5 +1,15 @@
 package gpms.accesscontrol;
 
 public class EditUserTest {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Accesscontrol ac = new Accesscontrol();
+		ac.initBalana();
+		String decision = ac.getXACMLdecision("Admin", "Edit","AllowsStaffView");
+		System.out.println(decision);
+	}
 
 }
