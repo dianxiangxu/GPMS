76ab007e7c20325ba46a16566fe14153aa9109b0
thomasvolz <thomasvolz@u.boisestate.edu>
2015-07-13 13:43:52 -0600

    Adding proposal creation and a suite to run that and the 100 user creation

---
+++ src/test/java/gpms/dao/test/Create100Proposals.java
@@ -0,0 +1,14 @@
+package gpms.dao.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Create100Proposals {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/gpms/dao/test/SUITECreateXUsersAndProposals.java
@@ -0,0 +1,14 @@
+package gpms.dao.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SUITECreateXUsersAndProposals {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
