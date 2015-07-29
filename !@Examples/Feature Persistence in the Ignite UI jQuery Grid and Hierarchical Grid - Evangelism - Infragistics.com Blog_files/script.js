9c5e41c36826f7dcea5c114391b55ddf9fc1ca72
Milson Munakami <milsonmun@yahoo.com>
2015-07-28 19:34:43 -0600

    example for Login form

---
+++ !@Examples/Feature Persistence in the Ignite UI jQuery Grid and Hierarchical Grid - Evangelism - Infragistics.com Blog_files/script.js
@@ -0,0 +1,20 @@
+﻿/* Author:
+
+*/
+
+/* add the tab id to the url so it can be shared.
+*/
+function loadTabImages(event, ui) {
+	$('img', ui.panel).trigger('imageShow');
+	if (event.type == 'tabsselect') {
+		document.location.hash = ui.panel.id;
+	}
+}
+function accordionScrollToSelected(event, ui) {
+	if (!ui.options.active) { return; }
+	var scrollPosition = ui.newHeader.offset().top - 1;
+	$('html:not(:animated),body:not(:animated)').animate(
+        { scrollTop: scrollPosition },
+        1000
+    );
+}
